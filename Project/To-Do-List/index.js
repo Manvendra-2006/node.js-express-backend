184028ffb71486a08ea0d9343e965f0c5b024c81
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-03-10 19:26:13 +0530

    feat: add multi-delete functionality and improve UI styling

--- Project/To-Do-List/index.js
+++ Project/To-Do-List/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import path from 'path'
 import { MongoClient, ObjectId } from 'mongodb'
 const dbName = 'TaskInfo'
 const URL = "mongodb://localhost:27017"
 const client = new MongoClient(URL)
 await client.connect()
 const app = express()
+const absPath = path.resolve('public')
+app.use(express.static(absPath))
 app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs")
 app.get("/",async (req,resp)=>{
@@ -59,5 +62,24 @@ app.post("/update/:id",async (req,resp)=>{
         else{
             resp.send("Error")
         }
+})
+app.post("/multi-delete",async (req,resp)=>{
+    const db = client.db(dbName)
+    const collection = db.collection('to-do')
+    console.log(req.body.selectedtask)
+    let selectedtask = undefined;
+    if(Array.isArray(req.body.selectedtask)){
+selectedtask = req.body.selectedtask.map((id)=> new ObjectId(id))
+    }
+    else{
+        selectedtask = [new ObjectId(req.body.selectedtask)]
+    }
+    const result = await collection.deleteMany({_id : {$in:selectedtask}}) // $in operator takes only array 
+    if(result){
+        resp.redirect("/")
+    }
+    else{
+        resp.send("error")
+    }
 })
 app.listen(1300)
